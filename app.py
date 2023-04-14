b169b5d41260c8275e237f970e16260ed97440a7
SHAHNAB AHMED <31092371+Shahnab@users.noreply.github.com>
2023-04-14 14:26:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ col1, mid, col2= st.columns([1,1,20])
 with col1:
     st.image("https://cdn-icons-png.flaticon.com/512/122/122482.png", width=70)
 with col2:
-    st.markdown("# Carlsberg Concierge: Your Personal Life, Business and Career Coach")
+    st.markdown("#### Carlsberg Concierge: Your Personal Life, Business and Career Coach")
 
 # embed streamlit docs in a streamlit app
-st.components.v1.iframe("https://ora.sh/embed/691f153b-12db-4243-b1d2-2eb069118ffc", width=1500, height=550, scrolling=True)
+st.components.v1.iframe("https://ora.sh/embed/691f153b-12db-4243-b1d2-2eb069118ffc", width=1250, height=500, scrolling=True)
